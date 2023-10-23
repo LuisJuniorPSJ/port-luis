a363774474ad619ca1da47737d68b9e03a437abd
Luis <luisfabiano_junior@hotmail.com>
2023-10-23 19:30:47 -0300

    Final-Project

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   contentVisible: boolean = false;
 
+  name: string = 'Luis Junior';
+
   ngOnInit(): void {
     setTimeout(() => {
       this.contentVisible = true;
