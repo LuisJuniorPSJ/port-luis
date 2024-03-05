c9fd200c8ddf79e7e1133d0911d2d8dde65261f5
Luis <luisfabiano_junior@hotmail.com>
2024-03-05 12:04:22 -0300

    att-port

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   contentVisible: boolean = false;
 
-  name: string = 'Luis Junior';
+  name: string = 'Luis Fabiano';
 
   ngOnInit(): void {
     setTimeout(() => {
